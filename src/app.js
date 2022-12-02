a4dddb74b64501681e3863b075a4527f4f91364a
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2022-12-02 11:11:21 +0000

    finalizado no responsive

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cardSuit = ["♦", "♥", "♠", "♣"];
+
+  let number = document.querySelector("#number");
+  number.style.fontSize = "150px";
+  let suit = document.querySelectorAll(".suit");
+
+  let randomNumberIndex = Math.floor(Math.random() * cardNumber.length);
+  number.innerHTML = cardNumber[randomNumberIndex];
+
+  let randomSuitIndex = Math.floor(Math.random() * cardSuit.length);
+  for (let i = 0; i < suit.length; i++) {
+    if (randomSuitIndex <= 1) {
+      suit[i].style.color = "red";
+    }
+
+    suit[i].innerHTML = cardSuit[randomSuitIndex];
+    suit[i].style.fontSize = "100px";
+  }
 };
+
+// Agregar = si sale la carta ya no pueda salir
+// agregar el boton
+// agregar que si sale la ultima carta me diga no hay mas cartas
